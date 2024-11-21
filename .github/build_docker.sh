1358c4d021fa98b0d7fa66cb9f9e96602a97f5b9
Omar Elhefnawy <omar.ehab.elhefnawy@gmail.com>
2024-11-21 17:49:08 +0100

    Fixing microservices

--- .github/build_docker.sh
+++ .github/build_docker.sh
@@ -19,19 +19,24 @@ done
 registry="${registry:-}"
 
 # Building all necessary Docker containers
-docker build -t "${registry}teastore-db" ../utilities/tools.descartes.teastore.database/
-docker build -t "${registry}teastore-kieker-rabbitmq" ../utilities/tools.descartes.teastore.kieker.rabbitmq/
-docker build -t "${registry}teastore-base" ../utilities/tools.descartes.teastore.dockerbase/
+docker build -t "${registry}teastore-db" .github/utilities/tools.descartes.teastore.database/
+docker build -t "${registry}teastore-kieker-rabbitmq" .github/utilities/tools.descartes.teastore.kieker.rabbitmq/
+docker build -t "${registry}teastore-base" .github/utilities/tools.descartes.teastore.dockerbase/
 
 # Update FROM statements in Dockerfiles to replace registry name if provided
-perl -i -pe's|.*FROM descartesresearch/|FROM '"${registry}"'|g' ../services/tools.descartes.teastore.*/Dockerfile
+perl -i -pe's|.*FROM descartesresearch/|FROM '"${registry}"'|g' .github/services/tools.descartes.teastore.*/Dockerfile
 
-docker build -t "${registry}teastore-registry" ../services/tools.descartes.teastore.registry/
-docker build -t "${registry}teastore-persistence" ../services/tools.descartes.teastore.persistence/
-docker build -t "${registry}teastore-image" ../services/tools.descartes.teastore.image/
-docker build -t "${registry}teastore-webui" ../services/tools.descartes.teastore.webui/
-docker build -t "${registry}teastore-auth" ../services/tools.descartes.teastore.auth/
-docker build -t "${registry}teastore-recommender" ../services/tools.descartes.teastore.recommender/
+
+docker build -t "${registry}teastore-registry" .github/services/tools.descartes.teastore.registry/
+docker build -t "${registry}teastore-persistence" .github/services/tools.descartes.teastore.persistence/
+docker build -t "${registry}teastore-image" .github/services/tools.descartes.teastore.image/
+docker build -t "${registry}teastore-webui" .github/services/tools.descartes.teastore.webui/
+docker build -t "${registry}teastore-auth" .github/services/tools.descartes.teastore.auth/
+docker build -t "${registry}teastore-recommender" .github/services/tools.descartes.teastore.recommender/
+perl -i -pe's|.*FROM '"${registry}"'|FROM descartesresearch/|g' .github/services/tools.descartes.teastore.*/Dockerfile
+
+
+
 
 # Reset FROM statement back to the original after build if needed
 perl -i -pe's|.*FROM '"${registry}"'|FROM descartesresearch/|g' ../services/tools.descartes.teastore.*/Dockerfile
